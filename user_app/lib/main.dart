2dd53b8d95e9d0ad57f6e6be868c797cd3ad850a
AmanPuthawala <76113162+AmanPuthawala@users.noreply.github.com>
2024-03-06 17:10:34 +0530

    user side added

---
+++ user_app/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:user_app/authentication/login_screen.dart';
+// Import the generated file
+import 'firebase_options.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await Permission.locationWhenInUse.isDenied.then((valueOfPermission){
+    if(valueOfPermission) {
+      Permission.locationWhenInUse.request();
+    }
+  });
+
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.appAttest,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
+
+
