b25817577cb807714f72b8b3c40be20f4155d017
Ujef94 <87478279+Ujef94@users.noreply.github.com>
2024-03-08 18:06:15 +0530

    Add the header for module

--- admin_uber_with_panel/lib/pages/trips_page.dart
+++ admin_uber_with_panel/lib/pages/trips_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../methods/common_methods.dart';
+
 class TripsPage extends StatefulWidget {
   static const String id = "\webPageTrips";
 
@@ -10,16 +12,49 @@ class TripsPage extends StatefulWidget {
 }
 
 class _TripsPageState extends State<TripsPage> {
+  CommonMethods cMethods = CommonMethods();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Trips Page",
-          style: TextStyle(
-            color: Colors.pinkAccent,
-            fontSize: 24
-        ),),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  "Manage Trips",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 18,
+              ),
+
+              Row(
+                children: [
+                  cMethods.header(2, "TRIP ID"),
+                  cMethods.header(1, "RIDER NAME"),
+                  cMethods.header(1, "DRIVER NAME"),
+                  cMethods.header(1, "CAR DETAILS"),
+                  cMethods.header(1, "TIMING"),
+                  cMethods.header(1, "FARE"),
+                  cMethods.header(1, "VIEW DETAILS"),
+                ],
+              ),
+
+              //Display data
+
+            ],
+          ),
+        ),
       ),
     );
   }
